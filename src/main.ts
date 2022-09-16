2c6464aac858fa42735d947c9827af493ae9c178
William Abreu <weabreu16@gmail.com>
2022-09-15 23:27:56 -0400

    feat: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create( AppModule, { cors: true } );
   const logger = new Logger('Bootstrap');
 
   app.useGlobalPipes(
@@ -13,7 +14,15 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT);
+  const config = new DocumentBuilder()
+    .setTitle('Emotions APP API')
+    .setVersion('0.1')
+    .build();
+  
+  const document = SwaggerModule.createDocument( app, config );
+  SwaggerModule.setup( '', app, document );
+
+  await app.listen( process.env.PORT );
   logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
