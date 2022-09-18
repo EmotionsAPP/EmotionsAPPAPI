5f90ee005c36536c872bb803718930f194e81a4c
William Abreu <weabreu16@gmail.com>
2022-09-17 20:40:08 -0400

    test: load and stress test

---
+++ test/load.test.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { createPsychologist } from './data';
+
+const { email, password } = createPsychologist;
+
+export let options = {
+    insecureSkipTLVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        { duration: '5m', target: 100 },
+        { duration: '10m', target: 100 },
+        { duration: '5m', target: 0 }
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<2000']
+    },
+};
+
+export default () => {
+    let response = http.post('http://localhost:3000/auth/login/psychologists', { email, password });
+};
---
+++ test/stress.test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { createPsychologist } from './data';
+
+const { email, password } = createPsychologist;
+
+export let options = {
+    insecureSkipTLVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 100 },
+        { duration: '5m', target: 200 },
+        { duration: '2m', target: 200 },
+        { duration: '5m', target: 300 },
+        { duration: '2m', target: 300 },
+        { duration: '5m', target: 400 },
+        { duration: '2m', target: 400 },
+        { duration: '5m', target: 0 }
+    ],
+};
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export default () => {
+    
+    http.batch([
+        ['POST', `${API_BASE_URL}/auth/login/psychologists`, { email, password }],
+        ['GET', `${API_BASE_URL}/articles`],
+        ['GET', `${API_BASE_URL}/appointments?userId=632164d855f8482c3dbe2804&date=2022-09-13`]
+    ]);
+
+};
