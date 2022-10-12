cf8573968323736b7cec26707436ec0cef7d121f
weabreu16 <weabreu16@gmail.com>
2022-10-11 23:39:15 -0400

    feat: rate limiter
    Using @nestjs/throttler to avoid brute-force attacks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { SeedModule } from './seed/seed.module';
@@ -14,6 +16,11 @@ import { CallingsModule } from './callings/callings.module';
 
     MongooseModule.forRoot(process.env.MONGODB),
 
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10
+    }),
+
     AuthModule,
     
     UsersModule,
@@ -26,5 +33,11 @@ import { CallingsModule } from './callings/callings.module';
 
     CallingsModule,
   ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
 export class AppModule {}
