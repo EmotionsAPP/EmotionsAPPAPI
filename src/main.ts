582ffbac54b4ff3ab662245e3b0eda2bca50a253
William Abreu <weabreu16@gmail.com>
2022-12-09 23:01:16 -0400

    feat: increase requests body size limit

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create( AppModule, { cors: true } );
   const logger = new Logger('Bootstrap');
+
+  app.use(json({ limit: process.env.BODY_SIZE_LIMIT }));
 
   app.useGlobalPipes(
     new ValidationPipe({
