9512dad18122ed55d59a002b7b7f1859bf800229
William Abreu <weabreu16@gmail.com>
2022-12-27 23:04:21 -0400

    fix: module express not found

--- .adminjs/.entry.js
+++ .adminjs/.entry.js
@@ -13,11 +13,11 @@ import Component5 from '../node_modules/.pnpm/@adminjs+logger@4.0.1_viqwgltehrmf
 AdminJS.UserComponents.Component5 = Component5
 import Component6 from '../node_modules/.pnpm/@adminjs+logger@4.0.1_viqwgltehrmfk656opsqrmlwzq/node_modules/@adminjs/logger/src/components/RecordLink'
 AdminJS.UserComponents.Component6 = Component6
-import Component7 from '../src/admin/components/property/PermissionSelectBox'
+import Component7 from '../dist/admin/components/property/PermissionSelectBox'
 AdminJS.UserComponents.Component7 = Component7
-import Component8 from '../src/admin/components/property/PropertyReferenceEdit'
+import Component8 from '../dist/admin/components/property/PropertyReferenceEdit'
 AdminJS.UserComponents.Component8 = Component8
-import Component9 from '../src/admin/components/property/PropertyReferenceEdit'
+import Component9 from '../dist/admin/components/property/PropertyReferenceEdit'
 AdminJS.UserComponents.Component9 = Component9
-import Component10 from '../src/admin/components/dashboard/Dashboard'
+import Component10 from '../dist/admin/components/dashboard/Dashboard'
 AdminJS.UserComponents.Component10 = Component10
